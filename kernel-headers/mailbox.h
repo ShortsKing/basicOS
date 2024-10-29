74b455ea6328e9b96546f8b09140e0956bd4bf9e
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2024-10-28 19:53:47 -0500

    Update mailbox.h
    revert to pre raspi detection

--- kernel-headers/mailbox.h
+++ kernel-headers/mailbox.h
@@ -2,37 +2,21 @@
 #include <stdint.h>
 
 static uint32_t MMIO_BASE;
-
 // The MMIO area base address, depends on board type
-static inline void mmio_init()
+static inline void mmio_init(int raspi)
 {
-	uint32_t reg;
-	char *board;
-
-	/* read the system register */
-#ifdef AARCH64
-	asm volatile ("mrs %x0, midr_el1" : "=r" (reg));
-#else
-	// assume aarch64 anyway ;)
-	asm volatile ("mrs 15,0,%0,c0,c0,0" : "=r" (reg));
-#endif
-
-	/* get the PartNum, detect board and MMIO base address */
-	switch ((reg >> 4) & 0xFFF) {
-		case 0xB76: board = "Rpi1"; MMIO_BASE = 0x20000000; break;
-		case 0xC07: board = "Rpi2"; MMIO_BASE = 0x3F000000; break;
-		case 0xD03: board = "Rpi3"; MMIO_BASE = 0x3F000000; break;
-		case 0xD08: board = "Rpi4"; MMIO_BASE = 0xFE000000; break;
-		default:    board = "????"; MMIO_BASE = 0x20000000; break;
-	}
+  switch (raspi) {
+    case 2:  MMIO_BASE = 0x3F000000; break; // for raspi2
+    case 3:  MMIO_BASE = 0x3F000000; break; // for raspi3 (same as raspi2)
+    case 4:  MMIO_BASE = 0xFE000000; break; // for raspi4
+    default: MMIO_BASE = 0x20000000; break; // for raspi1, raspi zero etc.
+  }
 }
-
 // Memory-Mapped I/O output
 static inline void mmio_write(uint32_t reg, uint32_t data)
 {
 	*(volatile uint32_t*)(MMIO_BASE + reg) = data;
 }
-
 // Memory-Mapped I/O input
 static inline uint32_t mmio_read(uint32_t reg)
 {
