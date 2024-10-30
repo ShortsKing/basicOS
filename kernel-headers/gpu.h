95c498d4c9afbc3235edd4bff5ff6e65725bce58
ShortsKing <82279627+ShortsKing@users.noreply.github.com>
2024-10-30 17:10:30 -0500

    Update gpu.h
    removed includes 
    all includes will now be in kernel.c

--- kernel-headers/gpu.h
+++ kernel-headers/gpu.h
@@ -1,5 +1,2 @@
-#include <stddef.h>
-#include <stdint.h>
-#include <mailbox.h>
-
+//will hold gpu functions like write_pixel and will also draw characters, as well as switching between graphical mode and textual mode.
 
